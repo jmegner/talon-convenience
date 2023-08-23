8558418e404908da8009c3fc4f712180a1757072
Jacob Egner <JacobEgner@example.com>
2023-08-23 08:58:01 -0500

    improve switch scripts

--- switch-to-community.ps1
+++ switch-to-community.ps1
@@ -1,8 +1,10 @@
 . $PSScriptRoot\dirs.ps1
 
-Move-Item -Path "$userDir\talon-config-jme" -Destination "$talonDir"
+Move-Item -Path "$myConfigActiveDir" -Destination "$talonDir"
 if (Test-Path -Path $communityArchiveDir) {
     Move-Item -Path "$communityArchiveDir" -Destination "$userDir"
 } else {
     git clone https://github.com/talonhub/community.git $communityActiveDir
 }
+
+& 'C:\Program Files\Talon\talon.exe'
