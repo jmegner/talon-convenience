053212286a48ea920575200d66cdc8675bcb7c2a
Jacob Egner <JacobEgner@example.com>
2023-09-15 14:15:55 -0500

    improve merge upstream

--- git-merge-upstream.ps1
+++ git-merge-upstream.ps1
@@ -4,25 +4,32 @@ try {
     $rootDir = "$rootParent\_talon"
     $tempConfigName = "talon-config-jme-$(Get-Date -Format 'yyyy-MM-dd_HHmm')"
     $tempConfigDir = "$rootDir\$tempConfigName"
+
     git clone --filter=blob:none https://github.com/jmegner/talon-config-jme.git "$tempConfigDir"
+
     Set-Location "$tempConfigDir"
     git remote add upstream https://github.com/talonhub/community.git
     git pull upstream main
     $pullSuccess = $?
+
+    # only the hashes
     $localTipCommit = git log -n 1 --format="%H"
     $remoteOldTipCommit = git log -n 1 --format="%H" origin/main
 
     if ($pullSuccess) {
         git status
         git push origin main
+        Set-Location $rootDir
+
         if ($localTipCommit -eq $remoteOldTipCommit) {
-            Write-Host "no conflicts and no pushed changes; you can delete $tempConfigName"
+            Write-Host "no conflicts and no pushed changes; will delete $tempConfigName"
         }
         else {
-            Write-Host "no conflicts and pushed some changesr; pull from origin into user and delete $tempConfigName"
+            Write-Host "no conflicts and pushed some changes; will pull from origin into user and delete $tempConfigName"
+            Invoke-Expression "$PSScriptRoot\git-fast-forward-all.ps1"
         }
-        Set-Location $rootDir
-        explorer $rootDir
+
+        Remove-Item -Recurse -Force $tempConfigDir
     }
     else {
         Write-Warning "merge conflicts; resolve them, push from $tempConfigName, pull to user, and delete temp folder"
